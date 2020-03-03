09717885e352d021df0ea20703d2085654b24312
acifuentes <acifuentes@kernet.es>
2020-03-03 22:30:30 +0100

    finished v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PreciosComponent } from './components/precios/precios.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { AuthGuard } from './services/auth.guard';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'precios', component: PreciosComponent},
-  {path: 'protegida', component: ProtegidaComponent},
+  {path: 'protegida', component: ProtegidaComponent, canActivate: [AuthGuard]},
   {path: '**', pathMatch: 'full', redirectTo: 'home'},
 
 ];
--- src/app/components/protegida/protegida.component.ts
+++ src/app/components/protegida/protegida.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-protegida',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProtegidaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
+    console.log('ngoinit protegida');
+    this.auth.userProfile$.subscribe(profile =>{
+      console.log(profile);
+    })
   }
 
 }
